baa9483570a43f2990111eb94c53560e625c9b22
ldbasilio <157418831+ldbasilio@users.noreply.github.com>
2025-09-03 02:37:58 +0000

    update

---
+++ 01 Python Introduction/01.07 School Desks.py
@@ -0,0 +1,11 @@
+scriptOne = "Please enter the # of students for 3 different classrooms :)"
+
+print (scriptOne)
+
+classA = float(input("Classroom A: "))
+classB = float(input("Classroom B: "))
+classC = float(input("Classroom C: "))
+
+classRooms = (int(classA), int(classB), int(classC))
+
+print (classRooms)
