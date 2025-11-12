8fdca788024539c9e4190453dbe3c743425c1c40
ldbasilio <157418831+ldbasilio@users.noreply.github.com>
2025-11-12 15:48:16 +0000

    update

--- 06 Files and Functions/06.00.08 - Functions.py
+++ 06 Files and Functions/06.00.08 - Functions.py
@@ -1,1 +1,13 @@
+"""
+Functions are the code sections which are isolated from the rest of the program and executed only when called. You've already met such functions as sqrt(), len() and print(). They all have something in common: they take parameters (zero, one, or several of them), and they can return a value (or they can return nothing). For example, the function sqrt() accepts one parameter and returns a value (the square root of the given number). The print() function can take various number of arguments and returns nothing. 
 
+Now we want to show you how to write a function called factorial() which takes a single parameter — the number, and returns a value — the factorial of that number. The def statement defines the name of the function and the name of values passed to it. The return statement returns the value calculated by the function.
+"""
+
+def factorial(n):
+    res = 1
+    for i in range(1, n + 1):
+        res *= i
+    return res
+ 
+print(factorial(3))
--- 06 Files and Functions/06.00.09 - How Does a Function Work - Part 1.py
+++ 06 Files and Functions/06.00.09 - How Does a Function Work - Part 1.py
@@ -1,1 +1,15 @@
+"""We want to provide a few explanations. First, the function code should be placed in the beginning of the program (before the place where we want to use the function factorial(), to be precise). The first line def factorial(n): of this example is a description of our function; the word factorial is an identifier (the name of our function). Right after the identifier, there goes the list of parameters that our function receives (in parentheses). The list consists of comma-separated identifiers of the parameters; in our case, there's only one parameter n. At the end of this line, put colon.
 
+Then goes the function body. In Python, the body must be indented (by Tab or four spaces, as always). This function calculates the value of n! and stores it in the variable res (meaning "result"). The last line of the function is return res, which exits the function and returns the value of the variable res.
+
+The return statement can appear in any place of a function. Its execution exits the function and returns specified value to the place where the function was called. If the function does not return a value, the return statement won't actually return some value (though it still can be used). Some functions do not need to return values, and the return statement can be omitted for them.
+"""
+
+def factorial(n):
+    res = 1
+    for i in range(1, n + 1):
+        res *= i
+    return res
+ 
+print(factorial(3))
+print(factorial(5))
